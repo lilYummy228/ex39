c1bf7e125d886837e042aef518b2d64548e47673
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-10-04 11:15:04 +0500

    Добавьте файлы проекта.

---
+++ ex39/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex39
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Себилла", "Эльф", "Вор", 6);
+
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        public string Name;
+        public string Race;
+        public string Classification;
+        public int Level;
+
+        public Player(string name, string race, string classification, int level)
+        {
+            Name = name;
+            Race = race;
+            Classification = classification;
+            Level = level;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя: {Name}\nРаса: {Race}\nКласс: {Classification}\nУровень: {Level}");
+        }
+    }
+}
